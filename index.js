5962fc2cae57fffb2754e3d3be67c73376d2c3ef
Vani Aggarwal <vaniaggarwal96@gmail.com>
2024-11-14 13:14:39 +0530

    Headover to console to play the game; Automated the game using for loop to play continuesly till round 5; debugged the code to tackle if user inputs anything other than the 3 options

--- index.js
+++ index.js
@@ -12,54 +12,52 @@ let humanMove = '';
     }
     return computerMove;
 }
-getComputerChoice()
+
 
 let getHumanChoice = () => {
     humanMove = prompt("Rock Paper Scissors. Choose & type one!");
     return humanMove? humanMove : '';
 }
-getHumanChoice()
 
 
-    let humanChoice = 0;
-    let computerChoice = 0;
-    let tie = 0;
-
-
+let humanChoice = 0;
+let computerChoice = 0;
+let tie = 0;
+let round = 0;
 let Result = "";
 
 function declareResult(){  
  humanMove = humanMove.toUpperCase()
-        if (computerMove === 'paper' && humanMove === 'PAPER'){
-            Result = "tie";
-        } else if (computerMove === 'rock' && humanMove=== 'ROCK'){
-            Result = "tie";
-        } else if (computerMove === 'scissors' && humanMove === 'SCISSORS'){
-            Result = "tie";
-        }
-        
-        else if (computerMove === 'paper' && humanMove === 'SCISSORS'){
-            Result = "win";
-        } else if (computerMove === 'rock' && humanMove === 'PAPER'){
-            Result = "win";
-        } else if (computerMove === 'scissors' && humanMove === 'ROCK'){
-            Result = "win";
-        }
-        
-        else if (computerMove === 'paper' && humanMove === 'ROCK'){
-            Result = "lose";
-        } else if (computerMove === 'rock' && humanMove === 'SCISSORS'){
-            Result = "lose";
-        } else if (computerMove === 'scissors' && humanMove === 'PAPER'){
-            Result = "lose";
-        }
+ if (humanMove === 'PAPER' || humanMove === 'ROCK' || humanMove === 'SCISSORS'){
+    if (computerMove === 'paper' && humanMove === 'PAPER'){
+        Result = "tie";
+    } else if (computerMove === 'rock' && humanMove=== 'ROCK'){
+        Result = "tie";
+    } else if (computerMove === 'scissors' && humanMove === 'SCISSORS'){
+        Result = "tie";
+    }
+    
+    else if (computerMove === 'paper' && humanMove === 'SCISSORS'){
+        Result = "win";
+    } else if (computerMove === 'rock' && humanMove === 'PAPER'){
+        Result = "win";
+    } else if (computerMove === 'scissors' && humanMove === 'ROCK'){
+        Result = "win";
+    }
+    
+    else if (computerMove === 'paper' && humanMove === 'ROCK'){
+        Result = "lose";
+    } else if (computerMove === 'rock' && humanMove === 'SCISSORS'){
+        Result = "lose";
+    } else if (computerMove === 'scissors' && humanMove === 'PAPER'){
+        Result = "lose";
+    }}
+    else{Result ='undefined'}
 }
-declareResult();
-console.log(Result)
 
 function score(){
     if (Result === 'tie') {
-      tie += 1;
+      tie ++;
     } else if(Result === 'win'){
       humanChoice ++;
     } else if(Result === 'lose'){
@@ -68,10 +66,19 @@ function score(){
         "error"
     }
 }
-score();
-console.log(`I chose : ${computerMove} 
-You chose : ${humanMove}
-${Result}
-Your Score : ${humanChoice} , My Score: ${computerChoice}  , Tie : ${tie}`)
 
-
+function playRound(){
+    for (let i = 1; i <=5; i++){
+    getComputerChoice();
+    getHumanChoice();
+    declareResult();
+    score();
+    round++
+    console.log(` Round number = ${round}
+    I chose : ${computerMove} 
+    You chose : ${humanMove}
+    ${Result}
+    Your Score : ${humanChoice} , My Score: ${computerChoice}  , Tie : ${tie}`
+    )}
+}
+playRound()
