2d021eb509baa9bc50f5000fd4ec56f38149b7f5
Vani Aggarwal <vaniaggarwal96@gmail.com>
2024-11-23 13:48:30 +0530

    cleaned up the code

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ let humanMove = '';
  let getComputerChoice= ()=> {
     randomMove = Math.random();
     if(randomMove >= 0 && randomMove <= 1/3){
-      computerMove = 'rock';
+      computerMove = 'ROCK';
     } else if(randomMove >= 1/3 && randomMove <= 2/3){
-        computerMove = 'paper';
+        computerMove = 'PAPER';
     }else if (randomMove >= 2/3 && randomMove <= 1){
-        computerMove = 'scissors'
+        computerMove = 'SCISSORS'
     }
     return computerMove;
 }
@@ -30,8 +30,8 @@ let getHumanChoice = () => {
 }
 console.log(humanMove);
 
-let humanChoice = 0;
-let computerChoice = 0;
+let humanScore = 0;
+let computerScore = 0;
 let tie = 0;
 let round = 0;
 let Result = "";
@@ -39,64 +39,63 @@ let Result = "";
 function declareResult(){  
  humanMove = humanMove.toUpperCase()
  if (humanMove === 'PAPER' || humanMove === 'ROCK' || humanMove === 'SCISSORS'){
-    if (computerMove === 'paper' && humanMove === 'PAPER'){
-        Result = "tie";
-    } else if (computerMove === 'rock' && humanMove=== 'ROCK'){
-        Result = "tie";
-    } else if (computerMove === 'scissors' && humanMove === 'SCISSORS'){
-        Result = "tie";
+    if (computerMove === 'PAPER' && humanMove === 'PAPER'){
+        Result = "WE TIE";
+    } else if (computerMove === 'ROCK' && humanMove=== 'ROCK'){
+        Result = "WE TIE";
+    } else if (computerMove === 'SCISSORS' && humanMove === 'SCISSORS'){
+        Result = "WE TIE";
     }
     
-    else if (computerMove === 'paper' && humanMove === 'SCISSORS'){
-        Result = "win";
-    } else if (computerMove === 'rock' && humanMove === 'PAPER'){
-        Result = "win";
-    } else if (computerMove === 'scissors' && humanMove === 'ROCK'){
-        Result = "win";
+    else if (computerMove === 'PAPER' && humanMove === 'SCISSORS'){
+        Result = "YOU WIN";
+    } else if (computerMove === 'ROCK' && humanMove === 'PAPER'){
+        Result = "YOU WIN";
+    } else if (computerMove === 'SCISSORS' && humanMove === 'ROCK'){
+        Result = "YOU WIN";
     }
     
-    else if (computerMove === 'paper' && humanMove === 'ROCK'){
-        Result = "lose";
-    } else if (computerMove === 'rock' && humanMove === 'SCISSORS'){
-        Result = "lose";
-    } else if (computerMove === 'scissors' && humanMove === 'PAPER'){
-        Result = "lose";
+    else if (computerMove === 'PAPER' && humanMove === 'ROCK'){
+        Result = "YOU LOSE";
+    } else if (computerMove === 'ROCK' && humanMove === 'SCISSORS'){
+        Result = "YOU LOSE";
+    } else if (computerMove === 'SCISSORS' && humanMove === 'PAPER'){
+        Result = "YOU LOSE";
     }}
     else{Result ='undefined'}
 }
 
 function score(){
-    if (Result === 'tie') {
+    if (Result === 'WE TIE') {
       tie ++;
-    } else if(Result === 'win'){
-      humanChoice ++;
-    } else if(Result === 'lose'){
-      computerChoice ++;
+    } else if(Result === 'YOU WIN'){
+      humanScore ++ ;
+    } else if(Result === 'YOU LOSE'){
+      computerScore ++;
     }else{
         "error"
     }
 }
 
 function playRound(){
-   // for (let i = 1; i <=5; i++){
+   for (let i = 1; i <=1; i++){
     getComputerChoice();
     getHumanChoice();
     declareResult();
     score();
-   // round++
-   display.innerHTML = `I chose : ${computerMove} 
-    You chose : ${humanMove}
-    ${Result}
-    Your Score : ${humanChoice} , My Score: ${computerChoice}  , Tie : ${tie}`
-}
-
-function displayResult(){
-   
-}
+    round ++;
+   display.innerHTML = 
+`<p>Round : ${round}</p>
+<p>I chose : ${computerMove} </p>
+<p>You chose : ${humanMove} </p>
+<p>${Result}</p>
+<p>Your Score : ${humanScore} , My Score: ${computerScore}  , Tie : ${tie}</p>`
+}}
 
 
-rock.addEventListener('click' , playRound);
-paper.addEventListener('click' , playRound);
-scissors.addEventListener('click' , playRound);
-//display.addEventListener('click' , displayResult);
+['rock', 'paper', 'scissors'].forEach(id => {
+    document.getElementById(id).addEventListener('click', playRound);
+});
+
+
   
